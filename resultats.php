cbc58e55917586aa89dd02d8a9c2b1b8406b095a
Alex <alexben@live.fr>
2021-11-15 09:12:07 +0100

    reagencement

--- resultats.php
+++ resultats.php
@@ -1,13 +1,11 @@
 <?php
+    $name = $_POST["name"];
+    $firstName = $_POST["firstName"];
+    $mail = $_POST["mail"];
+    $motif = $_POST["motif"];
+    $commentaire = $_POST["message"];
     
-
     if (isset($_POST[("submit")])) {
-        $name = $_POST["name"];
-        $firstName = $_POST["firstName"];
-        $mail = $_POST["mail"];
-        $motif = $_POST["motif"];
-        $commentaire = $_POST["message"];
-
         echo "Nom: $name <br>";
         echo "Prénom: $firstName <br>";
         echo "E-mail: $mail <br>";
